a61ef92b9f7593cdd0e36f33e5dc52045546ddd2
saiful178 <60336175+saiful178@users.noreply.github.com>
2020-02-17 12:43:09 +0600

    Add files via upload

---
+++ RESUME.php
@@ -0,0 +1,57 @@
+<html>
+<head><title>RESUME<title></head>
+<style>
+<p>
+{
+border 5px solid green
+}
+</style>
+</p>
+<center>
+<h1><u> My Resume <u><h1>
+</center>
+<h3>Saiful Islam</h3>
+<img src="ab.jpg" width="100" height="100" hspace="300" align="right">
+
+Bashabo,Shabugbagh,Dhaka<br><br>
+Mobile No:01969450489 br<br>
+Email Adress:Saiful.riyad178@gamil.com<br><br><hr>
+<h2>
+<div style="background-color:gray;color:white;padding:05px;">
+    <u>CARRER OBJECTIVE </u></h2>
+To succeed in an environment of growth and excellence and earn a job
+which provides me job satisfaction and self development and help me achivebr
+personal as well as organization goals.</div>
+<h2>Personal Information</h2>
+<ul>
+<li>Father Name:Shaheb Ali</li>
+<li>Mother Name:Razia Easmin</li>
+<li>Date of Birth:06.12.1999</li>
+<li>Nationality:Bangladeshi</li>
+<li>Religion:Islam</li>
+<li>Marital Status:Single</li>
+</ul>
+<h2>Qualification</h2>
+<table border="1" cellpadding="20%" cellspacing="30%"><tr><th>Examination</th><th>GPA</th><th>Board/University</th><th>Year of passing</th></tr>
+<tr><td>SSC</td><td>4.28</td><td>Comilla</td><td>2015<</td></tr>
+<tr><td>HSC</td><td>4.08</td><td>Dhaka</td><td>2017</td></tr>
+<tr><td>B.sc(Computer Science)</td><td>2.9</td><td>AIUB</td><td>7th seme</td></tr>
+</table>
+<h2>
+<u>STRENGTH</u>
+<h2>
+<ul>
+    
+    <li>Good Communication and inter-personal skill</li>
+    <li>Focused and Confident With positive attiude</li>
+    <li>Good team player</li>
+    <li> Self Motivated</li>
+    
+    
+<ul>
+<h2><u>Personal Skill:</u></h2>
+<ul><li>MS Word,Excel,Power Point,Visual Studio</li><li>Web Desining</li>
+<ul><li>Video Editing</li></ul>
+<h2><u>Language</u></h2>
+<ul><li>English</li><li>Bangla</li><li>Urdu</li><li>Hindi</li></ul>
+</ul></th></tr></table></body></p></html>
---
+++ RESUMEs.php
@@ -0,0 +1,66 @@
+<html>
+<head><title>RESUME</title></head>
+<style>
+<p>
+{
+border 5px solid green
+}
+</style>
+</p>
+<center>
+<h1><u> My Resume </u></h1>
+</center>
+<h2>Saiful Islam</h2>
+<img src="ab.png" width="150" height="150" hspace="100" align="right">
+
+Bashabo,Shabugbagh,Dhaka<br><br>
+Mobile No:01969450489 <br><br>
+Gmail Adress:Saiful.riyad178@gamil.com<br><br><hr>
+<h2>
+<div style="background-color:gray;color:white;padding:05px;">
+    <u>CARRER OBJECTIVE </u></h2>
+To succeed in an environment of growth and excellence and earn a job
+which provides me job satisfaction and self development and help me achivebr
+personal as well as organization goals.</div>
+<h2><u>Personal Information</u></h2>
+<ul>
+<li>Father Name:Shaheb Ali</li>
+<li>Mother Name:Razia Easmin</li>
+<li>Date of Birth:06.12.1999</li>
+<li>Nationality:Bangladeshi</li>
+<li>Religion:Islam</li>
+<li>Marital Status:Single</li>
+</ul>
+
+
+<h2><u>Emails and Social Sites:</u></h2>
+<ul>
+<li><b>Gmail:saiful.riyad178@gmail.com</b></li>
+<li><b>Email:saiful.riyad178@gmail.com</b></li>
+<li><b>Github:<a href="https://github.com/saiful178">https://github.com/saiful178</a></b></li>
+</ul>
+
+<h2><u>Qualification:</u></h2>
+<table border="1" cellpadding="20%" cellspacing="30%"><tr><th>Examination</th><th>GPA</th><th>Board/University</th><th>Year of passing</th></tr>
+<tr><td>SSC</td><td>4.28</td><td>Comilla</td><td>2015</td></tr>
+<tr><td>HSC</td><td>4.08</td><td>Dhaka</td><td>2017</td></tr>
+<tr><td>B.sc(Computer Science)</td><td>2.9</td><td>AIUB</td><td>7th seme</td></tr>
+</table>
+<h2>
+<u>STRENGTH:</u>
+</h2>
+<ul>
+    
+    <li>Good Communication and inter-personal skill</li>
+    <li>Focused and Confident with positive attitude</li>
+    <li>Good team player</li>
+    <li> Self Motivated</li>
+    
+    
+</ul>
+<h2><u>Personal Skill:</u></h2>
+<ul><li>MS Word,Excel,Power Point,Visual Studio</li><li>Photo Editing</li>
+<li>Video Editing</li><li>C</li><li>C++</li><li>JAVA</li><li>C#</li></ul>
+<h2><u>Language:</u></h2>
+<ul><li>English</li><li>Bangla</li><li>Urdu</li><li>Hindi</li></ul>
+</ul></h2></center></html>
